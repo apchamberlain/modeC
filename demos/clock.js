374824af11548d853dd53a74a9807104635f851b
Alex Chamberlain <apchamberlain@gmail.com>
2013-07-10 10:16:28 -0700

    Small improvements to clock demo

--- demos/clock.js
+++ demos/clock.js
@@ -1,27 +1,30 @@
-var colors = { body: '#ffd700', hands: '686868', face: '#f0f0f8'};
+var roygbiv = ['#FF0000',
+	       '#FF7300',
+	       '#FFFF00',
+	       '#33FF00',
+	       '#00FFE1',
+	       '#0000FF',
+	       '#9900FF'];
+
+// Setup
+
+var colors = { body: 'a0522d', chrome: '#ffd700', hands: '686868', face: '#f0f0f8'};
 
 var xmax = 250,
     ymax = xmax;
+
 var mC = modeC({ resolution: {x: xmax, y: ymax}, divname: 'modeC' });
 
 var centerx = Math.round(xmax / 2),
     centery = centerx;
 var outer_radius = Math.round((xmax / 2) * .8);
-var body_thickness = Math.round(outer_radius / 20 );
+var chrome_thickness = Math.round(outer_radius / 20 );
 
 mC.color('body', colors.body);   // Color table indices don't have to be numbers.
-mC.setcolor('body');
-
-for (var t = outer_radius; t > outer_radius - body_thickness; t--) {
-    mC.circle(centerx, centery, t, 3);
-}
-
+mC.color('chrome', colors.chrome);
+mC.color('hands', colors.hands);
 mC.color('face', colors.face);
-mC.setcolor('face');
 
-for (; t > 0; t--) {
-    mC.circle(centerx, centery, t, 3);
-}
 
 function degToRad(deg) {
     return (deg / 360) * 2 * Math.PI;
@@ -43,44 +46,72 @@ function drawHands(modeCobj, centerx, centery, length, color, hourangle, minutea
     modeCobj.line(centerx, centery, Math.round(minute_tip.x), Math.round(minute_tip.y));
 }
 
-mC.color('hands', colors.hands);
-mC.setcolor('hands');
+// Draw static elements
 
-drawHands(mC, centerx, centery, (outer_radius - body_thickness) * .9, 'hands', 0, 0);
+mC.setcolor('body');
+
+mC.rectangle(0, 0, xmax - 1, ymax - 1);
+
+mC.setcolor('chrome');
+
+for (var t = outer_radius; t > outer_radius - chrome_thickness; t--) {
+    mC.circle(centerx, centery, t, 3);
+}
+
+// Hour markers
+for (var a = 0; a < 360; a += 30) {
+    drawHands(mC, centerx, centery, outer_radius,
+	  'hands', degToRad(a), degToRad(a));
+}
+
+mC.setcolor('face');
+
+for (; t > 0; t--) {
+    mC.circle(centerx, centery, t, 3);
+}
 
 
 
 (function loopWrapper() {
+    // Sometimes it makes sense to minimize passing parameters around
+    // but avoid polluting the global namespace by declaring a bunch
+    // of closures that all act on a set of variables global to them
+    // and only them.
+
     var hourangle,
 	minuteangle,
 	oldhourangle,
 	oldminuteangle;
 
-    hourangle = 0;
-    minuteangle = 0;
+    var currtime = new Date();
+    var oldtime = currtime;
 
-    var currtime;
+    var getHandAngles = function() {
+	minuteangle = degToRad((currtime.getMinutes() / 60) * 360)
+	    - Math.PI / 2;
+	hourangle = degToRad(((currtime.getHours() % 12 ) / 12) * 360)
+	    + minuteangle/12  - Math.PI / 2;
+    };
 
-    var oldtime = new Date();
+    getHandAngles();
+
+    // draw hands in initial position
+    drawHands(mC, centerx, centery, (outer_radius - chrome_thickness) * .9,
+	      'hands', hourangle, minuteangle);
 
     var runClock = function()
     {
-	console.log("runClock");
 	currtime = new Date();
 	if (currtime - oldtime >= 60 * 1000) { // A minute has passed.
-	    console.log("\tupdate");
 	    oldtime = currtime;
 	    oldhourangle = hourangle;
 	    oldminuteangle = minuteangle;
-	    minuteangle = degToRad((currtime.getMinutes() / 60) * 360) - Math.PI / 2;
-	    hourangle = degToRad(((currtime.getHours() % 12 ) / 12) * 360) + minuteangle/12  - Math.PI / 2;
-	    console.log("\t\tnew hour = " + currtime.getHours() + ", angle = " + hourangle);
-	    console.log("\t\tnew minute = " + currtime.getMinutes() + ", angle = " + minuteangle);
+	    getHandAngles();
 	    // erase old hands
-	    drawHands(mC, centerx, centery, (outer_radius - body_thickness) * .9,
+	    drawHands(mC, centerx, centery, (outer_radius - chrome_thickness) * .9,
 		      'face', oldhourangle, oldminuteangle);
 	    // draw new ones
-	    drawHands(mC, centerx, centery, (outer_radius - body_thickness) * .9,
+	    drawHands(mC, centerx, centery, (outer_radius - chrome_thickness) * .9,
 		      'hands', hourangle, minuteangle);
 	}
     };
